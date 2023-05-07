cc425ee0aa130383ebed5fb29b1562514d5b5352
silvioukoth <silvioukoth@gmail.com>
2023-05-07 20:05:18 +0000

    add solutions

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,8 @@
-#!/usr/bin/node
-//Characters of a Star Wars movie:
+!/usr/bin/node
 
-const url = "https://swapi-api.alx-tools.com/api/starships/9/" + process.argv[2];
-const request = require("request");
+const request = require('request');
 
-request(url, async (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  response = JSON.parse(body);
-  for (const character of response.characters) {
-    await new Promise((resolve, reject) => {
-      request(character, (error, response, body) => {
-        if (error) {
-          console.log(error);
-        }
-        console.log(JSON.parse(body).name);
-        resolve();
-      });
-    });
-  }
-request(https://swapi-api.alx-tools.com/api/starships/9/" + process.argv[2], function (err, res, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
   if (err) throw err;
   const actors = JSON.parse(body).characters;
   exactOrder(actors, 0);
