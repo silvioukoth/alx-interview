7ecb7cc304c758bef42774fbf41caeb9a3a0c39d
silvioukoth <silvioukoth@gmail.com>
2023-05-13 10:26:34 +0000

    add chansges

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""0-stats module
-"""
+'''a script that reads stdin line by line and computes metrics'''
+
+
 import sys
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
-cache = {
-        '200': 0, '301': 0, '400': 0, '401': 0,
-        '403': 0, '404': 0, '405': 0, '500': 0
-        }
-size = 0
-count = 0
-
-if __name__ == "__main__":
+try:
     for line in sys.stdin:
-        count += 1
-        try:
-            data = line.split()
-            status = data[-2]
-            size += int(data[-1])
-            if status in cache:
-                cache[status] += 1
-        except Exception:
-            pass
-        if count % 10 == 0:
-            print("File size: {}".format(size))
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
             for key, value in sorted(cache.items()):
-                if value:
-                    print("{}: {}".format(key, value))
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
